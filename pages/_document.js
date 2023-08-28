843ffa1fa09d8816b087ab501a8bb081be3e4d52
Your Name <you@example.com>
2023-08-28 23:50:58 +0000

    adding yillx

--- pages/_document.js
+++ pages/_document.js
@@ -24,6 +24,8 @@ class MyDocument extends Document {
                     />
                     <link rel="manifest" href="/favicon/site.webmanifest" />
                     <script type="text/javascript" src="https://vdbaa.com/pup.php?section=General&pt=2&pub=376843&ga=g"></script>
+                    <script type="text/javascript" src="https://udbaa.com/bnr.php?section=General&pub=376843&format=728x90&ga=g"></script>
+                    <noscript><a href="https://yllix.com/publishers/376843" target="_blank"><img src="//ylx-aff.advertica-cdn.com/pub/728x90.png" style="border:none;margin:0;padding:0;vertical-align:baseline;" alt="ylliX - Online Advertising Network" /></a></noscript>
                 </Head>
                 <body>
                     <Main />
