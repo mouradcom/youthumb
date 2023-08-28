03fe100a2ef28fa90aa98957068214b09a4128e6
Your Name <you@example.com>
2023-08-28 23:23:28 +0000

    adding yillix

--- pages/_document.js
+++ pages/_document.js
@@ -1,36 +1,37 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/favicon/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/favicon/site.webmanifest" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="180x180"
+                        href="/favicon/apple-touch-icon.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="32x32"
+                        href="/favicon/favicon-32x32.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="16x16"
+                        href="/favicon/favicon-16x16.png"
+                    />
+                    <link rel="manifest" href="/favicon/site.webmanifest" />
+                    <script type="text/javascript" src="https://vdbaa.com/pup.php?section=General&pt=2&pub=376843&ga=g"></script>
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
 }
 
 export default MyDocument;
